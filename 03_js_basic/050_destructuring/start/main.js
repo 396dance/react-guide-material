de6b8bbc63bef592de72b5a772eb583e44f4eefb
Mikuno Yasuhiro <396dance@gmail.com>
2024-02-03 12:18:00 +0900

    分割代入

--- 03_js_basic/050_destructuring/start/main.js
+++ 03_js_basic/050_destructuring/start/main.js
@@ -1,27 +1,29 @@
-const arry = ["配列1", "配列2", "配列3"];
-console.log(arry[0]);
-console.log(arry[2]);
+// 配列に対して分割代入を使う場合は順番が重要
+// const [a, , c] = ["配列1", "配列2", "配列3"];
+// console.log(a);
+// console.log(c);
 
-const obj = { x: "オブジェクト1", y: "オブジェクト2", z: "オブジェクト3" };
-console.log(obj.x);
-console.log(obj.y);
+// オブジェクトに対して分割代入を使う場合はプロパティ名で取ってくるので順番は気にしなくてよいが、名前を新しくつけることはできない
+// const { x, z } = { x: "オブジェクト1", y: "オブジェクト2", z: "オブジェクト3" };
+// console.log(x);
+// console.log(z);
 
 const arr = ["Japan", "Tokyo", "Shinjuku"];
 const objAddress = { country: "Japan", state: "Tokyo", city: "Shinjuku" };
 
-const fnArr = (arry) => {
-  console.log("---配列---");
-  console.log(`country: ${arry[0]}`);
-  console.log(`state: ${arry[1]}`);
-  console.log(`city: ${arry[2]}`);
-};
+// const fnArr = ([country, state, city]) => {
+//   console.log("---配列---");
+//   console.log(`country: ${country}`);
+//   console.log(`state: ${state}`);
+//   console.log(`city: ${city}`);
+// };
 
-const fnObj = (objAddr) => {
+const fnObj = ({ country, state, city }) => {
   console.log("---オブジェクト---");
-  console.log(`country: ${objAddr.country}`);
-  console.log(`state: ${objAddr.state}`);
-  console.log(`city: ${objAddr.city}`);
+  console.log(`country: ${country}`);
+  console.log(`state: ${state}`);
+  console.log(`city: ${city}`);
 };
 
-fnArr(arr);
+// fnArr(arr);
 fnObj(objAddress);
